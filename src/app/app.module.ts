7334b10e54c419ffae0487fdf1df89fe0dd9d22a
miguel <mmeraz@netasystems.net>
2018-11-25 10:26:19 -0600

    conflicto medio resuelto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,46 +11,35 @@ import { CatAreaComponent } from './catalogos/cat-area/cat-area.component';
 import { CatDocumentosComponent } from './catalogos/cat-documentos/cat-documentos.component';
 import { CatDoccandidatoComponent } from './catalogos/cat-doccandidato/cat-doccandidato.component';
 import { CatCompcandidatoComponent } from './catalogos/cat-compcandidato/cat-compcandidato.component';
-import { CatCompcandidatoidComponent } from './catalogos/cat-compcandidatoid/cat-compcandidatoid.component';
 import { CatContactclienteComponent } from './catalogos/cat-contactcliente/cat-contactcliente.component';
 import { CatClienteComponent } from './catalogos/cat-cliente/cat-cliente.component';
 import { CatCandidatoComponent } from './catalogos/cat-candidato/cat-candidato.component';
 import { CatSolicitudComponent } from './catalogos/cat-solicitud/cat-solicitud.component';
 import { CatConteccandidatoComponent } from './catalogos/cat-conteccandidato/cat-conteccandidato.component';
-import { CatConteccandidatoidComponent } from './catalogos/cat-conteccandidatoid/cat-conteccandidatoid.component';
 import { CatComphabilidadesComponent } from './catalogos/cat-comphabilidades/cat-comphabilidades.component';
 import { CatContactoComponent } from './catalogos/cat-contacto/cat-contacto.component';
 import { CatContecnicosComponent } from './catalogos/cat-contecnicos/cat-contecnicos.component';
 import { CatEntrevistaComponent } from './catalogos/cat-entrevista/cat-entrevista.component';
-import { CatEntrevistaidComponent } from './catalogos/cat-entrevistaid/cat-entrevistaid.component';
 import { CatEquipoComponent } from './catalogos/cat-equipo/cat-equipo.component';
 import { CatEstacandidatoComponent } from './catalogos/cat-estacandidato/cat-estacandidato.component';
 import { CatEquipsolComponent } from './catalogos/cat-equipsol/cat-equipsol.component';
 import { CatEstasolicitudComponent } from './catalogos/cat-estasolicitud/cat-estasolicitud.component';
 import { CatFuncionesComponent } from './catalogos/cat-funciones/cat-funciones.component';
 import { CatHabsolicitudComponent } from './catalogos/cat-habsolicitud/cat-habsolicitud.component';
-import { CatHabsolicitudidComponent } from './catalogos/cat-habsolicitudid/cat-habsolicitudid.component';
 import { CatIdiomacandidatoComponent } from './catalogos/cat-idiomacandidato/cat-idiomacandidato.component';
-import { CatIdiomacandidatoidComponent } from './catalogos/cat-idiomacandidatoid/cat-idiomacandidatoid.component';
 import { CatIdiomaComponent } from './catalogos/cat-idioma/cat-idioma.component';
 import { CatJornadalabComponent } from './catalogos/cat-jornadalab/cat-jornadalab.component';
 import { CatPercepcioncndaComponent } from './catalogos/cat-percepcioncnda/cat-percepcioncnda.component';
-import { CatPercepcioncndaidComponent } from './catalogos/cat-percepcioncndaid/cat-percepcioncndaid.component';
 import { CatPercepcioncndofrComponent } from './catalogos/cat-percepcioncndofr/cat-percepcioncndofr.component';
-import { CatPercepcioncndofridComponent } from './catalogos/cat-percepcioncndofrid/cat-percepcioncndofrid.component';
 import { CatPerfilComponent } from './catalogos/cat-perfil/cat-perfil.component';
 import { CatPrepercepcionComponent } from './catalogos/cat-prepercepcion/cat-prepercepcion.component';
 import { CatPrioridadComponent } from './catalogos/cat-prioridad/cat-prioridad.component';
 import { CatProyectoComponent } from './catalogos/cat-proyecto/cat-proyecto.component';
 import { CatSeguicandidatoComponent } from './catalogos/cat-seguicandidato/cat-seguicandidato.component';
-import { CatSeguicandidatoidComponent } from './catalogos/cat-seguicandidatoid/cat-seguicandidatoid.component';
 import { CatSeguisolicitudComponent } from './catalogos/cat-seguisolicitud/cat-seguisolicitud.component';
-import { CatSeguisolicitudidComponent } from './catalogos/cat-seguisolicitudid/cat-seguisolicitudid.component';
 import { CatSolidiomaComponent } from './catalogos/cat-solidioma/cat-solidioma.component';
-import { CatSolidiomaidComponent } from './catalogos/cat-solidiomaid/cat-solidiomaid.component';
 import { CatSolpercepcionesComponent } from './catalogos/cat-solpercepciones/cat-solpercepciones.component';
 import { CatConsolicitadoComponent } from './catalogos/cat-consolicitado/cat-consolicitado.component';
-import { CatConsolicitadoidComponent } from './catalogos/cat-consolicitadoid/cat-consolicitadoid.component';
 import { CatTipvacanteComponent } from './catalogos/cat-tipvacante/cat-tipvacante.component';
 import { CatUsuarioComponent } from './catalogos/cat-usuario/cat-usuario.component';
 import { routing, appRoutingProviders } from './app.routing';
@@ -60,7 +49,41 @@ import { routing, appRoutingProviders } from './app.routing';
     AppComponent,
     NavbarComponent,
     CatAreaComponent,
-    CreateComponent,
+    CatCandidatoComponent,
+    CatClienteComponent,
+    CatCompcandidatoComponent,
+    CatComphabilidadesComponent,
+    CatConsolicitadoComponent,
+    CatContactclienteComponent,
+    CatContactoComponent,
+    CatConteccandidatoComponent,
+    CatContecnicosComponent,
+    CatDoccandidatoComponent,
+    CatDocumentosComponent,
+    CatEntrevistaComponent,
+    CatEquipoComponent,
+    CatEquipsolComponent,
+    CatEstacandidatoComponent,
+    CatEstasolicitudComponent,
+    CatFuncionesComponent,
+    CatHabsolicitudComponent,
+    CatIdiomaComponent,
+    CatIdiomacandidatoComponent,
+    CatJornadalabComponent,
+    CatPercepcioncndaComponent,
+    CatPercepcioncndofrComponent,
+    CatPerfilComponent,
+    CatPrepercepcionComponent,
+    CatPrioridadComponent,
+    CatProyectoComponent,
+    CatSeguicandidatoComponent,
+    CatSeguisolicitudComponent,
+    CatSolicitudComponent,
+    CatSolidiomaComponent,
+    CatSolpercepcionesComponent,
+    CatTipvacanteComponent,
+    CatUsuarioComponent
+
   ],
 
   imports: [
